48d8153883203d21fca6d3c90704ea0052a3e1e5
Albert Xmas <xmaspiano@hotmail.com>
2019-02-18 18:07:12 +0800

    Walkthrough - Step 37.Content Density

--- webapp/controller/HelloDialog.js
+++ webapp/controller/HelloDialog.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/base/ManagedObject",
-	"sap/ui/core/Fragment"
-], function (ManagedObject, Fragment) {
+	"sap/ui/core/Fragment",
+	"sap/ui/core/syncStyleClass"
+], function (ManagedObject, Fragment, syncStyleClass) {
 	"use strict";
 
 	return ManagedObject.extend("sap.ui.demo.walkthrough.controller.HelloDialog", {
@@ -29,9 +30,11 @@ sap.ui.define([
 					id: oView.getId(),
 					name: "sap.ui.demo.walkthrough.view.HelloDialog",
 					controller: oFragmentController
-				}).then(function (oDialog) {
+				}).then(function (oDialog){
 					// connect dialog to the root view of this component (models, lifecycle)
 					oView.addDependent(oDialog);
+					// forward compact/cozy style into dialog
+					syncStyleClass(oView.getController().getOwnerComponent().getContentDensityClass(), oView, oDialog);
 					oDialog.open();
 				});
 			} else {
